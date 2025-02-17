4667be5f55f03c1ef8c80d132b8e931524ef089d
A.Sharifi <alizadasharifi2022@gmail.com>
2025-02-17 14:31:42 -0600

    feat: complete sidebar section

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from "react-router-dom";
+import Overview from "./pages/Overview";
+import Products from "./pages/Products";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <>
+      <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+        {/* ===========background ========== */}
+        <div className="fixed inset-0 z-0 ">
+          <div className="absolute inset-0 bg-gradient-to-br from-gray-800 via-gray-900 opacity-80  ">
+            <div className="absolute inset-0 backdrop-blur-sm "></div>
+          </div>
+        </div>
+
+        {/* =============sidebar section ========== */}
+        <Sidebar />
+        <Routes>
+          <Route element={<Overview />} path="/" />
+          <Route element={<Products />} path="/products" />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Overview.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Overview() {
+    return (
+      <h1 className="text-white">hello</h1>
+  );
+}
+
+export default Overview;
